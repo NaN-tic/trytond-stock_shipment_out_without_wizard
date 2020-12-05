439f76dfe549f428fc7b7ae0154905c2ed6e87ad
Albert Cervera i Areny <albert@nan-tic.com>
2020-12-05 01:05:58 +0100

    Pyflakes fixes.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .shipment import *
+from . import shipment
 
 
 def register():
     Pool.register(
-        ShipmentOut,
+        shipment.ShipmentOut,
         module='stock_shipment_out_without_wizard', type_='model')
